5c5d097c65d52dbcee4431784bf56e1795e71eb1
Ana Lívia Rüegger Saldanha <analiviaruegger@gmail.com>
2025-11-03 21:09:20 -0300

    setup inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # code-review
+
 Atividade 3 da disciplina Engenharia de Software
+
+
+## Grupo
+
+Ana Lívia Rüegger Saldanha
+Be Zilberman
+Gustavo Mota Bastos
+
+
+## Linguagem escolhida
+
+Python
---
+++ solucoes.py
@@ -0,0 +1,11 @@
+def sao_anagramas(string1, string2):
+    # TODO: implementar a lógica (Ana)
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    # TODO: implementar a lógica (Be)
+    pass
+
+def valida_cpf(cpf_string):
+    # TODO: implementar a lógica (Gustavo)
+    pass
